854ca2a419303a22a130b9547e4ac61d2994bf1b
Jeffrey Zhao <jezhao1999@gmail.com>
2016-08-24 23:40:26 -0400

    Install and setup Katex

--- app/Controllers/ProblemController.php
+++ app/Controllers/ProblemController.php
@@ -8,14 +8,17 @@ class ProblemController {
 
 	public static function list($f3, $params) {
 		$problems = new Problem();
+		$f3->set('title', 'Problem List');
 		$f3->set('problems', $problems->find(NULL, ['order' => '_id SORT_ASC']));
 		$f3->set('content', 'problems/list.html');
+		$f3->set('loadKatex', true);
 		echo(\Template::instance()->render('layout.html'));
 	}
 
 	public static function show($f3, $params) {
 		$problem = new Problem();
 		$problem->load(['@slug = ?', $params['slug']]);
+		$f3->set('title', $problem->name);
 		$f3->set('problem', $problem);
 		$f3->set('content', 'problems/show.html');
 		echo(\Template::instance()->render('layout.html'));
