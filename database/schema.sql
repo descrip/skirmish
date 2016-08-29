243062c5d9faa59d90cc411183cd5c45d51fcf22
Jeffrey Zhao <jezhao1999@gmail.com>
2016-08-29 15:55:38 -0400

    Restructure schema for id-based primary keys, subtasks

--- database/schema.sql
+++ database/schema.sql
@@ -17,49 +17,67 @@ CREATE TABLE verdicts (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name VARCHAR NOT NULL,
 	code VARCHAR NOT NULL,
-	accepted INTEGER NOT NULL
+	is_accepted INTEGER NOT NULL	-- Meant to be boolean
 );
 
 CREATE TABLE users (
-	username VARCHAR PRIMARY KEY NOT NULL,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	username VARCHAR UNIQUE NOT NULL,
 	email VARCHAR UNIQUE NOT NULL,
 	password VARCHAR NOT NULL
 );
 
 CREATE TABLE problems (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	name VARCHAR UNIQUE NOT NULL,
-	slug VARCHAR PRIMARY KEY NOT NULL,
+	slug VARCHAR UNIQUE NOT NULL,
 	body TEXT NOT NULL
+);
+
+CREATE TABLE subtasks (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	problem_id INTEGER NOT NULL,
+	FOREIGN KEY(problem_id) REFERENCES problems(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE testcases (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	input TEXT NOT NULL,
-	output TEXT NOT NULL,		-- TODO: Support for program-based checking?
-	subtask_number INTEGER NOT NULL,
-	problem_slug VARCHAR NOT NULL,
-	FOREIGN KEY(problem_slug) REFERENCES problems(slug) ON UPDATE CASCADE ON DELETE CASCADE
+	output TEXT NOT NULL,	-- TODO: Support for program-based checking?
+	subtask_id INTEGER NOT NULL,
+	FOREIGN KEY(subtask_id) REFERENCES subtasks(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE submissions (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	points INTEGER NOT NULL,
-	problem_slug VARCHAR NOT NULL,
-	user_username INTEGER NOT NULL,
+	problem_id INTEGER NOT NULL,
+	user_id INTEGER NOT NULL,
 	verdict_id INTEGER NOT NULL,
 	language_id INTEGER NOT NULL,
-	FOREIGN KEY(problem_slug) REFERENCES problems(slug) ON UPDATE CASCADE,
-	FOREIGN KEY(user_username) REFERENCES users(username) ON UPDATE CASCADE,
+	FOREIGN KEY(problem_id) REFERENCES problems(id) ON UPDATE CASCADE,
+	FOREIGN KEY(user_id) REFERENCES users(id) ON UPDATE CASCADE,
 	FOREIGN KEY(verdict_id) REFERENCES verdicts(id) ON UPDATE CASCADE,
 	FOREIGN KEY(language_id) REFERENCES languages(id) ON UPDATE CASCADE
 );
 
-CREATE TABLE results (
+CREATE TABLE subtask_results (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	submission_id INTEGER NOT NULL,
+	subtask_id INTEGER NOT NULL,
+	FOREIGN KEY(submission_id) REFERENCES submissions(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	FOREIGN KEY(subtask_id) REFERENCES subtasks(id) ON UPDATE CASCADE
+);
+
+CREATE TABLE tescase_results (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	subtask_result_id INTEGER NOT NULL,
 	testcase_id INTEGER NOT NULL,
 	verdict_id INTEGER NOT NULL,
-	FOREIGN KEY(submission_id) REFERENCES submissions(id) ON UPDATE CASCADE,
+	FOREIGN KEY(subtask_result_id) REFERENCES subtask_results(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY(testcase_id) REFERENCES testcases(id) ON UPDATE CASCADE,
 	FOREIGN KEY(verdict_id) REFERENCES verdicts(id) ON UPDATE CASCADE
 );
+
+-- TODO: Create pivot table between users and problems for handling points.
+-- TODO: Create triggers for summing points.
